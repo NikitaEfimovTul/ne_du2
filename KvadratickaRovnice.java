1e3b118ab060667cfd023e698c0ca216307baa2c
NikitaEfimovTul <72413805+NikitaEfimovTul@users.noreply.github.com>
2020-10-25 13:49:32 +0100

    Add files via upload

---
+++ KvadratickaRovnice.java
@@ -0,0 +1,40 @@
+package efimov.apps;
+
+import java.util.Scanner;
+
+public class KvadratickaRovnice {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double a, b, c;
+        System.out.print("Zadejte koeficienty a, b, c: ");
+        a = sc.nextDouble();
+        b = sc.nextDouble();
+        c = sc.nextDouble();
+
+        double d, d2, re, im, x, x1, x2;
+        if (a == 0){
+            if (b == 0)
+                System.out.println("Tohle neni rovnice");
+            else if (b != 0){
+                x = -c / b;
+                System.out.println("Koren je: " + x);
+            }
+        }
+        else{
+            d = b * b - 4 * a * c;
+            d2 = Math.sqrt(Math.abs(d));
+            if (d < 0){
+                re = -b / (2 * a);
+                im = Math.abs(d2 / (2 * a));
+                System.out.format("x1 = %1$.3f + %2$.3f*i \nx2 = %1$.3f - %2$.3f*i\n", re, im);
+            }
+            else{
+                x1 = (-b + d2) / (2 * a);
+                x2 = (-b - d2) / (2 * a);
+                System.out.format("x1 = %1$.3f\nx2 = %2$.3f", x1, x2);
+            }
+        }
+    }
+}
